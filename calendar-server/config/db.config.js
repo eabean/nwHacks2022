c61f8bcc6f9806119b4b7899d7a7bf2471a1e7c3
Enid <edotadot2@gmail.com>
2022-01-15 15:18:28 -0800

    database set up

---
+++ calendar-server/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    "mongodb+srv://calenderuser:WORDpass@cluster0.pzvla.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+}
--- calendar-server/server.js
+++ calendar-server/server.js
@@ -3,6 +3,21 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 
 const app = express()
+//app.use(...)
+
+const db = require("./models")
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!")
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err)
+    process.exit()
+  })
 
 var corsOptions = {
   origin: "http://localhost:8081",
